ad224ad5faa6b6d9a4cae046f8a71e25bcbdbccc
zapahlevie <zapahlevie@gmail.com>
2019-04-07 19:44:38 +0700

    merging candidates

--- src/main/java/com/finalproject/automated/refactoring/tool/demo/service/implementation/DemoImpl.java
+++ src/main/java/com/finalproject/automated/refactoring/tool/demo/service/implementation/DemoImpl.java
@@ -5,6 +5,7 @@ import com.finalproject.automated.refactoring.tool.demo.service.Demo;
 import com.finalproject.automated.refactoring.tool.files.detection.model.FileModel;
 import com.finalproject.automated.refactoring.tool.files.detection.service.FilesDetection;
 import com.finalproject.automated.refactoring.tool.lazy.classes.detection.service.LazyClassDetection;
+import com.finalproject.automated.refactoring.tool.lazy.classes.merging.candidates.service.LazyClassMergingCandidates;
 import com.finalproject.automated.refactoring.tool.model.ClassModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,9 +27,14 @@ public class DemoImpl implements Demo {
     @Autowired
     private LazyClassDetection lazyClassDetection;
 
+    @Autowired
+    private LazyClassMergingCandidates lazyClassMergingCandidates;
+
     private static final Long LAZY_CLASS_THRESHOLD = 5L;
 
-    private Map<String, List<ClassModel>> lazyClasses = new LinkedHashMap<String, List<ClassModel>>();
+    private List<ClassModel> allClasses = new ArrayList<ClassModel>();
+    private List<ClassModel> lazyClasses = new ArrayList<ClassModel>();
+    private List<ClassModel> lazyClassCandidates = new ArrayList<ClassModel>();
 
     @Override
     public void doCodeSmellDetection(List<String> paths) {
@@ -50,17 +56,31 @@ public class DemoImpl implements Demo {
 
         System.out.println("Class detected -> " + classes.size());
 
-        classes.forEach(this::searchLazyClass);
+        classes.forEach(this::searchClasses);
+        classes.forEach(this::searchLazyClasses);
+        searchCandidates(allClasses, lazyClasses);
+
         System.out.println("Lazy Class detected -> " + lazyClasses.size());
+        System.out.println("Lazy Class Candidates -> " + lazyClassCandidates.size());
         System.out.println();
-//        lazyClasses.forEach(this::doPrintLazyClass);
-        classes.forEach(this::doPrintClass);
+        lazyClasses.forEach(this::doPrintLazyClass);
+//        classes.forEach(this::doPrintClass);
     }
 
-    private void searchLazyClass(String filename, List<ClassModel> classModels) {
+    private void searchCandidates(List<ClassModel> allClasses, List<ClassModel> lazyClasses) {
+        for(ClassModel source : lazyClasses){
+            lazyClassCandidates.add(lazyClassMergingCandidates.searchTarget(allClasses, source));
+        }
+    }
+
+    private void searchClasses(String s, List<ClassModel> classModels) {
+        this.allClasses.addAll(classModels);
+    }
+
+    private void searchLazyClasses(String s, List<ClassModel> classModels) {
         List<ClassModel> lazyClasses = lazyClassDetection.detect(classModels, LAZY_CLASS_THRESHOLD);
         if(lazyClasses.size() > 0){
-            this.lazyClasses.put(filename, lazyClasses);
+            this.lazyClasses.addAll(lazyClasses);
         }
     }
 
@@ -82,15 +102,20 @@ public class DemoImpl implements Demo {
 //            System.out.println("]");
 //            System.out.println("NOM : " + temp.getNom());
 //            System.out.println("NOF : " + temp.getNof());
+            System.out.println("LOC : " + temp.getLoc());
+            System.out.println("Path : " + temp.getPath());
 //            System.out.println();
         });
     }
 
-    private void doPrintLazyClass(String s, List<ClassModel> classModels) {
-        for(ClassModel classModel : classModels){
-            System.out.println("Lazy class detected -> " + classModel.getName());
-            System.out.println("NOM : " + classModel.getNom());
-            System.out.println("NOF : " + classModel.getNof());
+    private void doPrintLazyClass(ClassModel classModel) {
+        System.out.println("Lazy class detected -> " + classModel.getName());
+        System.out.println("NOM : " + classModel.getNom());
+        System.out.println("NOF : " + classModel.getNof());
+        System.out.println("LOC : " + classModel.getLoc());
+        ClassModel targetClass = lazyClassCandidates.get(lazyClasses.indexOf(classModel));
+        if(!(targetClass ==null)){
+            System.out.println("Target Class : " + targetClass.getPath() + "\\" + targetClass.getName() + ".java\n");
         }
     }
 }
